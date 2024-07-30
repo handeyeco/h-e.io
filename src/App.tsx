d60a42d70da6b1092021f1da63f4e2707cf2e1b1
handeyeco <matthew.c.tx@gmail.com>
2024-07-29 21:45:01 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="background"></div>
+    <div className="toolbar__container">handeyeco</div>
+    <div className="links__container">
+      <a href="https://www.handeye.co/">Art portfolio</a>
+      <a href="https://handeyeco.github.io/tech-blog/">Technical blog</a>
+      <a href="https://github.com/handeyeco">GitHub</a>
+      <a href="https://hyeco.bandcamp.com/">Bandcamp</a>
+      <a href="http://www.youtube.com/@_hyeco">YouTube</a>
+    </div>
+    </>
+  )
+}
+
+export default App
