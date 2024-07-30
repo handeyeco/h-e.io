3cdbe959f9a4ad16b7bda3fb21fa9fcefa1c46f9
handeyeco <matthew.c.tx@gmail.com>
2024-07-30 07:34:53 -0500

    tweak styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ function App() {
   return (
     <>
     <div className="background"></div>
-    <div className="toolbar__container">handeyeco</div>
+    <div className="toolbar__container">
+      <h1>handeyeco</h1>
+    </div>
     <div className="links__container">
       <a href="https://www.handeye.co/">Art portfolio</a>
-      <a href="https://handeyeco.github.io/tech-blog/">Technical blog</a>
-      <a href="https://github.com/handeyeco">GitHub</a>
       <a href="https://hyeco.bandcamp.com/">Bandcamp</a>
       <a href="http://www.youtube.com/@_hyeco">YouTube</a>
+      <a href="https://github.com/handeyeco">GitHub</a>
+      <a href="https://handeyeco.github.io/tech-blog/">Technical blog</a>
     </div>
     </>
   )
